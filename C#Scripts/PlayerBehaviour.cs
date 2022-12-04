e5190d87e263d5a3df9babec05908f01da6661c7
Bae-DongSU <109155242+Bae-DongSU@users.noreply.github.com>
2022-12-05 00:56:39 +0900

    Update PlayerBehaviour.cs

--- C#Scripts/PlayerBehaviour.cs
+++ C#Scripts/PlayerBehaviour.cs
@@ -1,55 +1,67 @@
 using UnityEngine;
 using System.Collections;
+using System;
 using UnityEngine.SceneManagement;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerBehaviour : MonoBehaviour
+{
+
+    public CoinManager eventSystem; // 코인 먹으면 없어지도
 
     Rigidbody2D rigid2D;
     Animator animator;
-    float jumpForce = 400.0f; //force가 너무 커서 점프하면 여러 블럭을 뛰어 넘어버리는 것을 볼 수 있었음.
-    float walkForce = 30.0f;
-    float maxWalkSpeed = 2.0f;
-    
-    void Start() {
+    float jumpForce = 680.0f;
+    float walkForce = 3.0f;
+    float maxWalkSpeed = 0.01f;
+
+    void Start()
+    {
         this.rigid2D = GetComponent<Rigidbody2D>();
         this.animator = GetComponent<Animator>();
+        eventSystem = GameObject.Find("Coin_Text").GetComponent<CoinManager>();
+        gameObject.tag = "player";
     }
-    
-    void Update() {
+
+    void Update()
+    {
         // 점프한다.
-        if (Input.GetKeyDown(KeyCode.Space) && this.rigid2D.velocity.y == 0) {
-              this.rigid2D.AddForce(transform.up * this.jumpForce);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
-        
+
         // 좌우 이동
         int key = 0;
-        if(Input.GetKey(KeyCode.RightArrow)) key = 1;
-        if(Input.GetKey(KeyCode.LeftArrow)) key = -1;
-        
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
         // 플레이어 속도
         float speedx = Math.Abs(this.rigid2D.velocity.x);
-        
+
         // 스피드 제한 
-        if(speedx < this.maxWalkSpeed) {
+        if (speedx < this.maxWalkSpeed)
+        {
             this.rigid2D.AddForce(transform.right * key * this.walkForce);
         }
-        
-        //움직이는 방향에 따라 반전
-        if(key != 0) {
+
+        if(key != 0)
+        {
             transform.localScale = new Vector3(key, 1, 1);
         }
 
-        //플레이어 속도에 맞춰 애니메이션 속도 바꾸기
         this.animator.speed = speedx / 2.0f;
-        
-        //플레이어가 화면 밖으로 나가면 처음부터
-        if(transform.position.y < -10) {
-            SceneManager.LoadScene("GameScene");
-        }
     }
-    
-    void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("골");
-        SceneManager.LoadScene("ClearScene");
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("coin"))
+        {
+            eventSystem.Add_coin();
+        }
+
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            SceneManager.LoadScene("EndScene");
+        }
     }
 }
